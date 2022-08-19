cdae0e6970e0132a8cc8359dd72d1f75d026e2e2
Park-Mi-Ran <gpfk6636@naver.com>
2022-08-19 20:55:49 +0900

    15

--- compiler/urls.py
+++ compiler/urls.py
@@ -1,9 +1,9 @@
 # from django.conf.urls import url
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.intro, name='intro'),
     path('aboutus',views.aboutus, name='aboutus'),
-    path('compiler', views.runCode, name="compiler"),
+    re_path(r'^compiler/$', views.runCode, name="compiler"),
 ]
