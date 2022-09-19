62d3278b19ace7dae98d1b836ef36fd9fb7271a0
Park-Mi-Ran <gpfk6636@naver.com>
2022-09-20 00:54:38 +0900

    view 및 수정

--- member/urls.py
+++ member/urls.py
@@ -5,10 +5,10 @@ from . import views
 app_name = 'member'
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='member/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='FE_tamplates/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.signup, name='signup'),
-    path('mypage/', views.mypage, name="mypage"),
+    path('mypage/', views.submitCodeCorrect, name="mypage"),
     path('mypage/submitCodeCorrect/', views.submitCodeCorrect, name="submitCodeCorrect"),
     path('mypage/likePost/', views.likePost, name="likePost"),
 ]
--- member/views.py
+++ member/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from member.forms import UserForm
 from post.models import Post
-
+from django.core.paginator import Paginator
 def signup(request):
     if request.method == "POST":
         form = UserForm(request.POST)
@@ -15,29 +15,52 @@ def signup(request):
             return redirect('/')
     else:
         form = UserForm()
-    return render(request, 'member/signup.html', {'form': form})
+    return render(request, 'FE_tamplates/singup.html', {'form': form})
 
-def mypage(request):
-    if request.user.is_authenticated:
-        login_user = request.user
+# def mypage(request):
+#     if request.user.is_authenticated:
+#         login_user = request.user
     
-    data = {
-        'username': login_user.username,
-        'email': login_user.email
-    }
-    return render(request, 'member/mypage.html', data)
+#     post_correct_list = Post.objects.all().filter(writer=login_user).order_by('-pub_date')
+#     post_like_list = Post.objects.all().filter(like_users__in=[login_user]).order_by('-pub_date')
+    
+
+#     paginator2=Paginator( post_like_list, 9)
+#     pagenum = request.GET.get('page')
+#     post_like_list=paginator2.get_page(pagenum)
+#     data = {
+#         'username': login_user.username,
+#         'email': login_user.email
+#     }
+#     return render(request, 'FE_tamplates/mypage.html', {'data':data,'post_correct_list': post_correct_list,'post_like_list':post_like_list})
 
 def submitCodeCorrect(request):
     if request.user.is_authenticated:
         login_user = request.user
 
     post_list = Post.objects.all().filter(writer=login_user).order_by('-pub_date')
-    return render(request, 'member/correctCode.html', {'post_list': post_list})
+
+    paginator1 = Paginator(post_list, 9)
+    pagenum = request.GET.get('page')
+    post_list=paginator1.get_page(pagenum)
+    data = {
+        'username': login_user.username,
+        'email': login_user.email
+    }    
+    return render(request, 'FE_tamplates/mypage.html', {'data':data,'post_list': post_list})
 
 def likePost(request):
     if request.user.is_authenticated:
         login_user = request.user
 
     post_list = Post.objects.all().filter(like_users__in=[login_user]).order_by('-pub_date')
-    return render(request, 'member/likePost.html', {'post_list': post_list})
+    
+    paginator1 = Paginator(post_list, 9)
+    pagenum = request.GET.get('page')
+    post_list=paginator1.get_page(pagenum)
+    data = {
+        'username': login_user.username,
+        'email': login_user.email
+    }
+    return render(request, 'FE_tamplates/likePost.html', {'data':data,'post_list': post_list})
 
