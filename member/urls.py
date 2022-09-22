b6a46b317582509120c2adc7bbb8e9846d57c7c1
Park-Mi-Ran <gpfk6636@naver.com>
2022-09-22 21:19:16 +0900

    mypage 수정

--- member/urls.py
+++ member/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='FE_tamplates/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.signup, name='signup'),
-    path('mypage/', views.submitCodeCorrect, name="mypage"),
+    path('mypage/', views.mypage, name="mypage"),
     path('mypage/submitCodeCorrect/', views.submitCodeCorrect, name="submitCodeCorrect"),
     path('mypage/submitCodeIncorrect/', views.submitCodeIncorrect, name="submitCodeIncorrect"),
     path('mypage/retryCode/<int:id>', views.retryCode, name="retryCode"),
--- member/views.py
+++ member/views.py
@@ -5,7 +5,7 @@ from django.core.paginator import Paginator
 from member.forms import UserForm
 from compiler.forms import CodeExecutorForm
 
-from post.models import Post
+from post.models import Post,Problem
 from compiler.models import UserCheck
 from problem.models import Problem, Example
 
@@ -25,22 +25,23 @@ def signup(request):
         form = UserForm()
     return render(request, 'FE_tamplates/singup.html', {'form': form})
 
-# def mypage(request):
-#     if request.user.is_authenticated:
-#         login_user = request.user
+def mypage(request):
+    if request.user.is_authenticated:
+        login_user = request.user
     
-#     post_correct_list = Post.objects.all().filter(writer=login_user).order_by('-pub_date')
-#     post_like_list = Post.objects.all().filter(like_users__in=[login_user]).order_by('-pub_date')
+    post_list = Post.objects.all().filter(writer=login_user.username).order_by('-pub_date')
+    post_correct_list = Post.objects.all().filter(writer=login_user).order_by('-pub_date')
+    post_like_list = Post.objects.all().filter(like_users__in=[login_user]).order_by('-pub_date')
+    incorrect_list = UserCheck.objects.all().filter(user=login_user, is_correct=0)
     
-
-#     paginator2=Paginator( post_like_list, 9)
-#     pagenum = request.GET.get('page')
-#     post_like_list=paginator2.get_page(pagenum)
-#     data = {
-#         'username': login_user.username,
-#         'email': login_user.email
-#     }
-#     return render(request, 'FE_tamplates/mypage.html', {'data':data,'post_correct_list': post_correct_list,'post_like_list':post_like_list})
+    # paginator2=Paginator( post_like_list, 6)
+    # pagenum = request.GET.get('page')
+    # post_like_list=paginator2.get_page(pagenum)
+    data = {
+        'username': login_user.username,
+        'email': login_user.email
+    }
+    return render(request, 'FE_tamplates/mypage.html', {'data':data,'incorrect_list':incorrect_list,'post_list':post_list,'post_correct_list': post_correct_list,'post_like_list':post_like_list})
 
 def submitCodeCorrect(request):
     if request.user.is_authenticated:
@@ -48,36 +49,30 @@ def submitCodeCorrect(request):
 
     post_list = Post.objects.all().filter(writer=login_user.username).order_by('-pub_date')
 
-    paginator1 = Paginator(post_list, 9)
-    pagenum = request.GET.get('page')
-    post_list=paginator1.get_page(pagenum)
-    data = {
-        'username': login_user.username,
-        'email': login_user.email
-    }    
-    return render(request, 'FE_tamplates/mypage.html', {'data':data, 'post_list': post_list})
-
-def submitCodeIncorrect(request):
-    if request.user.is_authenticated:
-        login_user = request.user
-
-    incorrect_list = UserCheck.objects.all().filter(user=login_user, is_correct=0)
-    for usercheck in incorrect_list:
-        print(usercheck.problem.id, '!!')
-    # print(incorrect_list.get().problem)
-    # print(incorrect_list, '!!!!!!!!!!!')
-
-    # post_list = Post.objects.all().filter(writer=login_user).order_by('-pub_date')
-
     # paginator1 = Paginator(post_list, 9)
     # pagenum = request.GET.get('page')
     # post_list=paginator1.get_page(pagenum)
     data = {
         'username': login_user.username,
         'email': login_user.email
+    }    
+    return render(request, 'FE_tamplates/correctCode.html', {'data':data, 'post_list': post_list})
+
+def submitCodeIncorrect(request):
+    if request.user.is_authenticated:
+        login_user = request.user
+    
+    
+    incorrect_list = UserCheck.objects.all().filter(user=login_user, is_correct=0)
+    for usercheck in incorrect_list:
+        print(usercheck.problem.id, '!!')
+
+    data = {
+        'username': login_user.username,
+        'email': login_user.email
     }
-    return render(request, 'member/incorrectCode.html', {'data':data, 'incorrect_list': incorrect_list})
-    # return render(request, 'FE_tamplates/mypage.html', {'data':data, 'incorrect_list': incorrect_list})
+    return render(request, 'FE_tamplates/incorrectCode.html', {'data':data, 'incorrect_list': incorrect_list})
+
 
 def retryCode(request, id):
     problem = get_object_or_404(Problem, pk=id)
